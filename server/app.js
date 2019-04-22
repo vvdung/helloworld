82fee840f96aaca3762deaa98f5643b801736211
vvdung <vvdung@hueuni.edu.vn>
2019-04-22 08:44:59 +0700

    Add files via upload

---
+++ server/app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+routes(app);
+
+var server = app.listen(3005, function () {
+    console.log("app running on port.", server.address().port);
+});
