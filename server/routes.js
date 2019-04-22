21a96863107b1c032a796479decfb1655de280b1
vvdung <vvdung@hueuni.edu.vn>
2019-04-22 09:26:06 +0700

    Add files via upload

--- server/routes.js
+++ server/routes.js
@@ -4,6 +4,23 @@ var appRouter = function (app) {
     res.status(200).send("Welcome to RESTFUL API - NODEJS - TINK39");
   });
 
+  app.post("/userinfo", function (req, res) {
+    res.status(200).send("USERINFO API");
+  });
+  
+  app.post("/login", function (req, res) {
+		var user = req.body.username;
+    var pass = req.body.password;	
+    if (user == "vvdung")
+    	res.status(200).send("HÀM ĐĂNG NHẬP [" + user + "/" + pass +"]");
+	  else		
+			res.status(200).send("LOGIN API [" + user + "/" + pass +"]");
+  });
+  
+  app.post("/register", function (req, res) {
+    res.status(200).send("REGISTER API");
+  });
+
 }
 
 module.exports = appRouter;
