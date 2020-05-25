c4b7c8b6b034b7322a912805a35c3c2a96395963
ohm21c <ohm21c@gmail.com>
2020-05-25 09:42:06 +0700

    1

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.helloworld;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.view.View;
 import android.widget.Toast;
 import android.content.Intent;
 
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
 
     EditText edtUser,edtPass; //Biến điều khiển EditText
     Button   btnLogin, btnRegister; //Biến điều khiển Button
@@ -29,10 +45,13 @@ public class MainActivity extends AppCompatActivity {
         btnRegister.setOnClickListener(new CButtonRegister());
     }
 
-    public class CButtonLogin implements View.OnClickListener {
+    public class CButtonLogin  implements View.OnClickListener {
 
         @Override
         public void onClick(View v) {//Hàm sử lý sự kiện click button login
+
+
+
             //Nhận nội dung từ biến điều khiển
             String szUser = edtUser.getText().toString();
             String szPass = edtPass.getText().toString();
@@ -44,17 +63,26 @@ public class MainActivity extends AppCompatActivity {
             }
 
             //Kiểm tra nếu tài khoản, mật khẩu chính xác
-            if (szUser.equalsIgnoreCase("vvdung") &&
+           /* if (szUser.equalsIgnoreCase("vvdung") &&
             szPass.equalsIgnoreCase("111111")){
                 //Toast.makeText(getApplicationContext(),"ACCOUNT OKIED!",Toast.LENGTH_SHORT).show();
                 userNameLogined = szUser;
                 Intent intent = new Intent(getApplicationContext(),UserActivity.class);
                 startActivity(intent);
                 return;
-            }
+            }*/
 
             String szMsg = "[" + szUser + "/"  + szPass + "]";
             Toast.makeText(getApplicationContext(),szMsg,Toast.LENGTH_SHORT).show();
+
+            try {
+                //doGet("http://172.16.1.1:3000/users");
+                String data = "{\"username\":\"" + szUser + "\",\"password\":\"" + szPass +"\"}";
+                Log.d("K40",data);
+                doPost("http://172.16.1.1:3000/login",data);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 
@@ -63,8 +91,59 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onClick(View v) {//Hàm sử lý sự kiện click button register
             //Toast.makeText(getApplicationContext(),"CButtonRegister::onClick...",Toast.LENGTH_SHORT).show();
-            Intent i = new Intent(getApplicationContext(), RegisterActivity.class);
-            startActivity(i);
+            //Intent i = new Intent(getApplicationContext(), RegisterActivity.class);
+            //startActivity(i);
+
+            try {
+                doGet("http://172.16.1.1:3000/users");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
+    }
+
+    void doGet(String url) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                call.cancel();
+            }
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                final String myResponse = response.body().string();
+                MainActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                       //txtString.setText(myResponse);
+                        Log.d("K40",myResponse);
+                    }
+                });
+            }
+        });
+    }
+
+    void doPost(String url,String json) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+        RequestBody body = RequestBody.create(JSON,json);
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                call.cancel();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                Log.d("K40",response.body().string());
+            }
+        });
     }
 }
